e14fdbb2b3f45e0684ab89aac82251e2fd8179c7
Jordan Hollinger <jordan.hollinger@gmail.com>
2015-08-28 11:25:00 -0400

    Update example apps

--- examples/complex/db/migrate/20140913065838_create_widgets.rb
+++ examples/complex/db/migrate/20140913065838_create_widgets.rb
@@ -2,7 +2,7 @@ class CreateWidgets < ActiveRecord::Migration
   def change
     create_table :widgets do |t|
       t.string :name, null: false
-      t.timestamps
+      t.timestamps null: false
     end
   end
 end
--- examples/complex/db/schema.rb
+++ examples/complex/db/schema.rb
@@ -15,8 +15,8 @@ ActiveRecord::Schema.define(version: 20140913065838) do
 
   create_table "widgets", force: :cascade do |t|
     t.string   "name",       null: false
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
