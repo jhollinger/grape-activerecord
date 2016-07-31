271041ff53538c18797e434ecddde0b683b02e3f
Jordan Hollinger <jordan.hollinger@gmail.com>
2016-07-31 12:09:57 -0400

    Shim fix

--- lib/grape/activerecord.rb
+++ lib/grape/activerecord.rb
@@ -1,8 +1,7 @@
-require 'grape/activerecord/version'
-
 # Shim for otr-activerecord
 require 'otr-activerecord'
 module Grape
   ActiveRecord = OTR::ActiveRecord
 end
+require 'grape/activerecord/version'
 $stderr.puts "WARNING: grape-activerecord is deprecated; please switch to otr-activerecord"
